737a388ee7e0f02bb57fadbaefeabc744e94d248
Kabilan <kabilanraja7@gmail.com>
2026-07-01 23:47:43 +0530

    feat: add endpoint discovery infrastructure

---
+++ src/TaskService/Common/Extensions/EndpointExtensions.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using TaskService.Common.Endpoints;
+
+namespace TaskService.Common.Extensions
+{
+    public static class EndpointExtensions
+    {
+        public static IServiceCollection AddEndpoints(this IServiceCollection services)
+        {
+            var endpointType = typeof(IEndpoint);
+
+            var endpoints = Assembly
+                .GetExecutingAssembly()
+                .DefinedTypes
+                .Where(type => endpointType.IsAssignableFrom(type)
+                                && !type.IsInterface
+                                && !type.IsAbstract);
+
+            foreach (var endpoint in endpoints)
+            {
+                services.AddTransient(endpoint);
+            }
+            return services;
+        }
+
+        public static WebApplication MapEndpoints(this WebApplication app)
+        {
+            var enpointType = typeof(IEndpoint);
+
+            var endpoints = app.Services
+                .GetServices(enpointType)
+                .Cast<IEndpoint>();
+
+            foreach (var endpoint in endpoints)
+            {
+                endpoint.MapEndpoint(app);
+            }
+            return app;
+        }
+    }
+}
